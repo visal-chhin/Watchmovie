e2a55e0248604a01f4e08176b7511c263d8f1b1a
Your Name <youremail@example.com>
2026-06-05 09:46:19 +0700

    Use Meta

--- movie.py
+++ movie.py
@@ -31,11 +31,12 @@ TOKEN = "8648355227:AAHcQQySFDT3EZvWRJ4rEh7nK7rTQXOp8qk"
 AJAX_ENDPOINT = "https://khdiamond.net/wp-admin/admin-ajax.php"
 BASE_REFERER = "https://khdiamond.net"
 
-HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36"
-    )
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/137.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://khdiamond.net/",
+    "Origin": "https://khdiamond.net",
 }
 
 POST_ID_REGEX = re.compile(r"postid-(\d+)")
@@ -271,10 +272,12 @@ async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
     try:
-        embed_url = get_stream(
-            page_url,
-            media_type
-        )
+        r.raise_for_status()
+    except Exception as e:
+        print("FAILED URL:", r.url)
+        print("STATUS:", r.status_code)
+        print(r.text[:500])
+        raise
 
         await query.message.reply_text(
             f"📺 {media_type.upper()}:\n"
